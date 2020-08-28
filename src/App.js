33da9084bca159d28a1f8ea2dc29bb021937389b
Subhashree Mohapatra <lipi.devops@gmail.com>
2020-08-28 06:13:00 +0000

    App.js file modifed on Aug 27 2020

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
            Modified  one more line in my feature  branch
         </p>
         <p>
-           Modified second line in staging branch
+           Modified second line in staging branch on August 27 2020
         </p>       
         <a
           className="App-link"
